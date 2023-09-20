927875efb0acfe5c947822850f82518ddda7db57
Duncan Sommerville <duncan.sommerville@illumio.com>
2023-09-20 13:09:08 -0400

    move constants into separate module

---
+++ TA-Illumio/bin/illumio_constants.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+"""This module provides constant values for the TA.
+
+Copyright:
+    © 2023 Illumio
+License:
+    Apache2, see LICENSE for more details.
+"""
+# sourcetypes
+SYSLOG_SOURCETYPE = "illumio:pce"
+HEALTH_SOURCETYPE = "illumio:pce:health"
+
+# KVStore names
+KVSTORE_IP_LISTS = "illumio_ip_lists"
+KVSTORE_LABELS = "illumio_labels"
+KVSTORE_RULE_SETS = "illumio_rule_sets"
+KVSTORE_RULES = "illumio_rules"
+KVSTORE_SERVICES = "illumio_services"
+KVSTORE_WORKLOADS = "illumio_workloads"
+KVSTORE_WORKLOAD_INTERFACES = "illumio_workload_interfaces"
+KVSTORE_PORT_SCAN = "illumio_port_scan_settings"
+
+# illumio types
+ILO_TYPE_IP_LISTS = "illumio:pce:ip_lists"
+ILO_TYPE_LABELS = "illumio:pce:labels"
+ILO_TYPE_RULE_SETS = "illumio:pce:rule_sets"
+ILO_TYPE_SERVICES = "illumio:pce:services"
+ILO_TYPE_WORKLOADS = "illumio:pce:workloads"
